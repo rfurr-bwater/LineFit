e0bcede09b07cbd0b265facdce6f8e5010fee529
Robert Furr <rfurr@eagles.bridgewater.edu>
2025-11-12 14:34:45 -0500

    Added Files

---
+++ fitting_functions.py
@@ -0,0 +1,10 @@
+def linear(m,x,b):
+    return (m*x)+b
+
+def slope_units(x_units,y_units):
+    y_units = y_units.rstrip('s')
+    x_units = x_units.rstrip('s')
+    return y_units + "/" + x_units
+
+def print_equation(m,b,x_units,y_units):
+    print(f"The equation of the line is: y = {m} {slope_units(x_units,y_units)} x + {b}")
---
+++ test_fxn.py
@@ -0,0 +1,5 @@
+from fitting_functions import *
+
+print_equation(2,3,"meters","kg")
+
+print_equation(4,1,"meters","kg")
